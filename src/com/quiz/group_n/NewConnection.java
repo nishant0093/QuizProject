c29c5f3549708047457af55a63a47a7d50dff71f
nileema <nileemabarwal7@gmail.com>
2023-01-28 02:12:36 +0530

    Display

--- src/com/quiz/group_n/NewConnection.java
+++ src/com/quiz/group_n/NewConnection.java
@@ -10,7 +10,7 @@ public class NewConnection {
 	public Connection newConnection() {
 		try {
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/users","root","Root");}
+		conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/users","root","root");}
 		catch(Exception e) {
 			e.printStackTrace();
 		}
